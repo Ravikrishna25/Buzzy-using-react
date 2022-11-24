1245454237569d4eb1fd4cd5b3692ab705cdac76
Ravikrishna25 <ravikrishna2919@gmail.com>
2022-11-24 14:53:03 +0530

    Second Commit
    Value Changed in HOME.js

--- src/home.js
+++ src/home.js
@@ -218,7 +218,7 @@ export default function Home() {
                 }}
                 align="center"
               >
-                SELECT BUS
+                SELECT BUSes
               </Button>
                 </Link>
           </Grid>
